53cf343b78824b2eb04951674c3c7a0393697786
Jose Sanchez <jose92ca@gmail.com>
2019-05-23 17:52:07 -0700

    Update 2 to MatrixText.cpp
    In this update, I created new text cases to test not only the printMatrix() function, but also the new functions I recently add, namely:
    setValue as well as the overloaded '+' operator which sums two matrices and returns the result. The code works properly for this demo.

--- MatrixTest.cpp
+++ MatrixTest.cpp
@@ -11,12 +11,28 @@
 
 int main()
 {
-    Matrix x(1, 4);
+    Matrix x(2, 4); //1 row, 4 columns
+    Matrix y(2, 4); //1 row, 4 columns
+    
+    y.setValue(0, 1, 3); // set row 0 (which is 1 in compsci), column 1 to 3
+    x.setValue(0, 2, 5); // set row 0, column 2 (really 3) to 5.
+    
+    Matrix z;
+    z = x + y; //make the new object created above (z) equal to the sum of the matrices
+    
+    cout << "First Matrix: " << endl << endl;
     
     x.printMatrix();
     
-    cout << "columns: " << x.getNumColumns() << endl;
-    cout << "rows: " << x.getNumRows() << endl;
+    cout << endl << endl << "+" << endl << endl;
+    
+    cout << "Second Matrix: " << endl << endl;
+    
+    y.printMatrix();
+    
+    cout << endl << endl << "=" << endl << endl;
+    
+    z.printMatrix();
     
     return 0;
 }
