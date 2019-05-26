ead6f8424f09509b88a92c27a2ea0d0b6a98d0d9
Jose Sanchez <jose92ca@gmail.com>
2019-05-25 20:42:19 -0700

    Update 4 (Corrected) Matrix.cpp
    I made a mistake and uploaded the wrong file. This is the corrected version, which now includes the transpose() function that transposes a matrix. Please ignore the last #4 update to this file.

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,119 +1,98 @@
 /*
- This file contains certain tests to ensure correct functionality of the Matrix class (both the Matrix.h and Matrix.cpp files).
- The comments above each section of code will describe what the code is meant to test, and the correct behavior that should be
- exhibited by the program.
- 
- Code Author: Jose Sanchez Rodriguez
+ Matrix.cpp
+ MatrixApp
+ This is the defined body of the Matrix class. It works in conjunction with the Matrix.h file to fully define the class.
+ It contains the function definitions.
  */
 
-#include <iostream>
+#include <stdio.h>
 #include "Matrix.h"
 
-int main()
+Matrix::Matrix(int rows, int columns)
 {
-    /*
-     The code should create two different Matrix objects ('x' and 'y').
-     The both objects should be instantiated it's 2D vector with 2 rows and 4 columns (2 x 4 matrices).
-     Furthermore, they should be filled with zeros.
-     */
-    Matrix x(2, 4);
-    Matrix y(2, 4);
+    vector<int> vect(columns,0);
     
-    /*
-     The code should print out both of the matrices (x and y, respectively), separated by two blank lines.
-     */
+    for(int i = 0; i < rows; i++)
+        matrix.push_back(vect);
+}
+
+void Matrix::printMatrix()
+{
+    for(int i = 0; i < matrix.size(); i++)
+    {
+        for(int j = 0; j < matrix[i].size(); j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        
+        cout << endl;
+    }
+}
+
+int Matrix::getNumRows()
+{
+    return matrix.size();
+}
+
+int Matrix::getNumColumns()
+{
+    return matrix[0].size();
+}
+
+int Matrix::getValue(int row, int column)
+{
+    return matrix[row][column];
+}
+
+void Matrix::setValue(int row, int column, int value)
+{
+    matrix[row][column] = value;
+}
+
+Matrix Matrix::operator+(Matrix &a) //this function (overloaded '+' operator) adds two matrices together
+{
+    Matrix sumMatrix(a.getNumRows(), a.getNumColumns());
     
-    x.printMatrix();
-    cout << endl << endl;
-    y.printMatrix();
-    cout << endl << endl;
+    for(int i = 0; i < a.getNumRows(); i++)
+    {
+        for(int j = 0; j < a.getNumColumns(); j++)
+        {
+            sumMatrix.matrix[i][j] = this->getValue(i, j) + a.getValue(i, j);
+        }
+    }
     
-    /*
-     (Keep in mind that we begin counting rows and columns at 0 for the next piece of code)
-     The code should set the value in matrix 'x', row 1, column 3, to 3.
-     It should also set the value in matrix 'y', row 1, column 2, to 5.
-     */
-    x.setValue(0, 2, 5);
-    y.setValue(0, 1, 3);
+    return sumMatrix;
+}
+
+Matrix Matrix::operator-(Matrix &a) //this function substracts to the right of it from the matrix to the left of it.
+{
     
-    /*
-     The code should simply print both matrices again to ensure the correct updates were made by the code above.
-     */
+    Matrix differenceMatrix(a.getNumRows(), a.getNumColumns());
     
-    x.printMatrix();
-    cout << endl << endl;
-    y.printMatrix();
-    cout << endl << endl;
+    for(int i = 0; i < a.getNumRows(); i++)
+    {
+        for(int j = 0; j < a.getNumColumns(); j++)
+        {
+            differenceMatrix.matrix[i][j] = this->getValue(i, j) - a.getValue(i, j);
+        }
+    }
     
-    /*
-     The code should create a new Matrix object 'z'. Since we are not passing it parameter, it should create an empty matrix,
-     since that is what the default values are in the constructor.
-     After creating the object, it should sum the 'x' and 'y' objects together, resulting in a new object which is then assigned to 'z'.
-     Thus, the '+' overload is being tested to ensure that the matrices of each object to the left and to the right of it are summed together.
-     */
-    Matrix z;
-    z = x + y;
+    return differenceMatrix;
+}
+
+Matrix Matrix::transpose()
+{
     
-    /*
-     The code should now print both our 'x' and 'y'(first and second) matrices, as well as their sum which should now be stored in 'z'.
-     */
-    cout << "First Matrix: " << endl << endl;
+    Matrix transposedMatrix(getNumColumns(), getNumRows());
     
-    x.printMatrix();
+    for(int i = 0; i < getNumRows(); i++)
+    {
+        for(int j = 0; j < getNumColumns(); j++)
+        {
+            transposedMatrix.matrix[j][i] = matrix[i][j];
+        }
+    }
     
-    cout << endl << endl << "   +" << endl << endl;
-    
-    cout << "Second Matrix: " << endl << endl;
-    
-    y.printMatrix();
-    
-    cout << endl << endl << "   =" << endl << endl;
-    
-    z.printMatrix();
-    
-    /*
-     The code creates a new Matrix object, 'w', with 2 rows and 4 columns. It then stores the values 7, 9, -2, and 2 inside of it.
-     Then it creates another Matrix object, 'diffWandZ', that stores the difference between matrices w and z.
-     It then prints out the computation and it's result, as the code above.
-     */
-    
-    Matrix w(2, 4);
-    
-    w.setValue(1, 0, 7);
-    w.setValue(0, 3, 9);
-    w.setValue(0, 1, -2);
-    w.setValue(0, 2, 2);
-    
-    Matrix diffWandZ = w - z;
-    
-    cout << endl << endl << "Matrix w: " << endl << endl;
-    
-    w.printMatrix();
-    
-    cout << endl << endl << "   -" << endl << endl;
-    
-    cout << "Matrix z: " << endl << endl;
-    
-    z.printMatrix();
-    
-    cout << endl << endl << "   =" << endl << endl;
-    
-    diffWandZ.printMatrix();
-    
-/*
-    The code transposes matrix z and prints it.
- */
-    cout << "\nMatrix z before transposition: " << endl << endl;
-    
-    z.printMatrix();
-    cout << endl << endl;
-    
-    cout << "Matrix z after transpositon: " << endl << endl;
-    
-    z = z.transpose();
-    z.printMatrix();
-    cout << endl << endl;
-    
-    return 0;
+    return transposedMatrix;
 }
 
