72ab2ecef3225a2c5434d2c8eb0d75db826eb528
Jose Sanchez <jose92ca@gmail.com>
2019-06-06 11:00:35 -0700

    Update 1 (new menu functions & quiz option)
    This update reflects the addition of new functions that make the code more readable and structured. It not contains the following four functions:
    
    void displayMainMenu();
    void displayMatrixOpsMenu();
    void takeAdditionQuiz();
    void takeMultiplicationQuiz();
    
    The titles are descriptive of what they do. Their existence assists with better program flow, so it is easier to see what is going on when reading the code. Also, this update allows users to take the multiplication matrix test.

--- MatrixAppMain.cpp
+++ MatrixAppMain.cpp
@@ -10,12 +10,28 @@
 #include <iostream>
 #include "Matrix.h"
 
+void displayMainMenu();
+void displayMatrixOpsMenu();
+void takeAdditionQuiz();
+void takeMultiplicationQuiz();
+
+
+
 int main()
+{
+    cout << "Hello, and welcome to my matrix calculator and quiz app!" << endl;
+    displayMainMenu();
+    
+    return 0;
+}
+
+
+
+void displayMainMenu()
 {
     string choice;
     
-    cout << "Hello, and welcome to my matrix calculator and quiz app!" << endl;
-    cout << "Please choose from the following options: " << endl << endl;
+    cout << "\nPlease choose from the following options: " << endl << endl;
     cout << "1. Matrix operations quiz" << endl;
     cout << "2. Matrix calculator (coming soon!)" << endl << endl;
     cout << "Your choice -> ";
@@ -33,87 +49,203 @@ int main()
     if(choice == "2")
         cout << "\nSorry, that feature has not been fully implemented yet. Check back soon!" << endl;
     
-    else //option must have been 1.
+    else //must have chose option 1
+        displayMatrixOpsMenu();
+}
+
+void displayMatrixOpsMenu()
+{
+    string choice;
+    
+    cout << "\nWhich matrix operation would you like to be quizzed on?\n" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Multiplication" << endl << endl;
+    cout << "Your choice -> ";
+    getline(cin, choice);
+    
+    while(choice != "1" && choice != "2")
     {
-        cout << "\nWhich matrix operation would you like to be quizzed on?\n" << endl;
+        cout << "\n\nInvalid option! Please choose from the options listed:" << endl << endl;
         cout << "1. Addition" << endl;
         cout << "2. Multiplication" << endl << endl;
         cout << "Your choice -> ";
         getline(cin, choice);
+    }
+    
+    if(choice == "1")
+    {
+        takeAdditionQuiz();
+    }
+    
+    else //must have been option 2 (multiplication)
+    {
+        takeMultiplicationQuiz();
+    }
+}
+
+void takeAdditionQuiz()
+{
+    srand((unsigned)(time(0))); //create seed for random number to produce a size for the matrices on the quiz.
+    int correct = 0;
+    int numQuestions = 0;
+    string keepGoing = "y";
+    int randomRowSize, randomColumnSize;
+    vector<int> nums;
+    Matrix sum;
+    string choice;
+    
+    while (keepGoing == "y")
+    {
+        randomRowSize = rand() % 3 + 2; //generate a random number between 2 and 6 inclusively.
+        randomColumnSize = rand() % 3 + 2;
         
-        while(choice != "1" && choice != "2")
+        Matrix a(randomRowSize, randomColumnSize);
+        Matrix b(randomRowSize, randomColumnSize);
+        a.randomFill();
+        b.randomFill();
+        sum = a + b;
+        Matrix answer(randomRowSize, randomColumnSize);
+        
+        cout << endl << numQuestions + 1 << "." << endl << endl;
+        a.printMatrix();
+        cout << endl << "+" << endl << endl;
+        b.printMatrix();
+        cout << endl << "=" << endl << endl;
+        
+        cout << "Please enter your answer below:" << endl << endl;
+        answer.getUserMatrix();
+        
+        if(!answer.isEqual(sum))
         {
-            cout << "\n\nInvalid option! Please choose from the options listed:" << endl << endl;
-            cout << "1. Addition" << endl;
-            cout << "2. Multiplication" << endl << endl;
-            cout << "Your choice -> ";
-            getline(cin, choice);
+            cout << "\nNice try, but that is not the correct answer." << endl;
+            cout << "The correct answer was: " << endl << endl;
+            sum.printMatrix();
         }
         
-        if(choice == "1")
+        else
         {
-            srand((unsigned)(time(0))); //create seed for random number to produce a size for the matrices on the quiz.
-            int correct = 0;
-            int numQuestions = 0;
-            string keepGoing = "y";
-            int randomRowSize, randomColumnSize;
-            vector<int> nums;
-            Matrix sum;
-            
-            while (keepGoing == "y")
-            {
-                randomRowSize = rand() % 3 + 2; //generate a random number between 2 and 6 inclusively.
-                randomColumnSize = rand() % 3 + 2;
-                
-                Matrix a(randomRowSize, randomColumnSize);
-                Matrix b(randomRowSize, randomColumnSize);
-                a.randomFill();
-                b.randomFill();
-                sum = a + b;
-                Matrix answer(randomRowSize, randomColumnSize);
-                
-                cout << endl << numQuestions + 1 << "." << endl << endl;
-                a.printMatrix();
-                cout << endl << "+" << endl << endl;
-                b.printMatrix();
-                cout << endl << "=" << endl << endl;
-
-                cout << "Please enter your answer below:" << endl << endl;
-                answer.getUserMatrix();
-                
-                if(!answer.isEqual(sum))
-                {
-                    cout << "\nNice try, but that is not the correct answer." << endl;
-                    cout << "The correct answer was: " << endl << endl;
-                    sum.printMatrix();
-                }
-                
-                else
-                {
-                    cout << "\nGreat job! :D" << endl << endl;
-                    correct++;
-                }
-                
-                numQuestions++;
-                
-                cout << "Would you like to try another question? ('y' or 'Y' for yes or 'n' or 'N' for no)" << endl;
-                cout << "Your choice -> ";
-                getline(cin, keepGoing);
-                keepGoing[0] = tolower(keepGoing[0]);
-                
-                while(keepGoing != "y" && keepGoing != "n")
-                {
-                    cout << "\n\nInvalid option! Please choose from the options listed:" << endl << endl;
-                    cout << "Would you like to try another question? ('y' or 'Y' for yes or 'n' or 'N' for no)" << endl;
-                    cout << "Your choice -> ";
-                    getline(cin, keepGoing);
-                    keepGoing[0] = tolower(keepGoing[0]);
-                }
-            }
-    
-            cout << "\nYour final score: " << (static_cast<double>(correct) / numQuestions) * 100 << "%" << endl;
+            cout << "\nGreat job! :D" << endl << endl;
+            correct++;
+        }
+        
+        numQuestions++;
+        
+        cout << "\nWould you like to try another question? ('y' or 'Y' for yes or 'n' or 'N' for no)" << endl;
+        cout << "Your choice -> ";
+        getline(cin, keepGoing);
+        keepGoing[0] = tolower(keepGoing[0]);
+        
+        while(keepGoing != "y" && keepGoing != "n")
+        {
+            cout << "\n\nInvalid option! Please choose from the options listed:" << endl << endl;
+            cout << "Would you like to try another question? ('y' or 'Y' for yes or 'n' or 'N' for no)" << endl;
+            cout << "Your choice -> ";
+            getline(cin, keepGoing);
+            keepGoing[0] = tolower(keepGoing[0]);
         }
     }
     
-    return 0;
+    cout << "\nYour final score: " << (static_cast<double>(correct) / numQuestions) * 100 << "%" << endl;
+    
+    cout << "\nPlease choose from the following options: " << endl << endl;
+    cout << "1. Return to main menu" << endl;
+    cout << "2. Quit program" << endl << endl;
+    cout << "Your choice -> ";
+    getline(cin, choice);
+    
+    while(choice != "1" && choice != "2")
+    {
+        cout << "\n\nInvalid option! Please choose from the following options::" << endl << endl;
+        cout << "1. Return to main menu" << endl;
+        cout << "2. Quit program" << endl << endl;
+        cout << "Your choice -> ";
+        getline(cin, choice);
+    }
+    
+    if(choice == "1")
+        displayMainMenu();
+}
+
+void takeMultiplicationQuiz()
+{
+    srand((unsigned)(time(0))); //create seed for random number to produce a size for the matrices on the quiz.
+    int correct = 0;
+    int numQuestions = 0;
+    string keepGoing = "y";
+    int randomRowSizeA, randomColumnSizeA, randomColumnSizeB;
+    vector<int> nums;
+    Matrix sum;
+    string choice;
+    
+    while (keepGoing == "y")
+    {
+        randomRowSizeA = rand() % 3 + 2; //generate a random number between 2 and 6 inclusively.
+        randomColumnSizeA = rand() % 3 + 2;
+        randomColumnSizeB = rand() % 3 + 2;
+        
+        Matrix a(randomRowSizeA, randomColumnSizeA);
+        Matrix b(randomColumnSizeA, randomColumnSizeB);
+        a.randomFill();
+        b.randomFill();
+        sum = a * b;
+        Matrix answer(randomRowSizeA, randomColumnSizeB);
+        
+        cout << endl << numQuestions + 1 << "." << endl << endl;
+        a.printMatrix();
+        cout << endl << "*" << endl << endl;
+        b.printMatrix();
+        cout << endl << "=" << endl << endl;
+        
+        cout << "Please enter your answer below:" << endl << endl;
+        answer.getUserMatrix();
+        
+        if(!answer.isEqual(sum))
+        {
+            cout << "\nNice try, but that is not the correct answer." << endl;
+            cout << "The correct answer was: " << endl << endl;
+            sum.printMatrix();
+        }
+        
+        else
+        {
+            cout << "\nGreat job! :D" << endl << endl;
+            correct++;
+        }
+        
+        numQuestions++;
+        
+        cout << "\nWould you like to try another question? ('y' or 'Y' for yes or 'n' or 'N' for no)" << endl;
+        cout << "Your choice -> ";
+        getline(cin, keepGoing);
+        keepGoing[0] = tolower(keepGoing[0]);
+        
+        while(keepGoing != "y" && keepGoing != "n")
+        {
+            cout << "\n\nInvalid option! Please choose from the options listed:" << endl << endl;
+            cout << "Would you like to try another question? ('y' or 'Y' for yes or 'n' or 'N' for no)" << endl;
+            cout << "Your choice -> ";
+            getline(cin, keepGoing);
+            keepGoing[0] = tolower(keepGoing[0]);
+        }
+    }
+    
+    cout << "\nYour final score: " << (static_cast<double>(correct) / numQuestions) * 100 << "%" << endl;
+    
+    cout << "\nPlease choose from the following options: " << endl << endl;
+    cout << "1. Return to main menu" << endl;
+    cout << "2. Quit program" << endl << endl;
+    cout << "Your choice -> ";
+    getline(cin, choice);
+    
+    while(choice != "1" && choice != "2")
+    {
+        cout << "\n\nInvalid option! Please choose from the following options::" << endl << endl;
+        cout << "1. Return to main menu" << endl;
+        cout << "2. Quit program" << endl << endl;
+        cout << "Your choice -> ";
+        getline(cin, choice);
+    }
+    
+    if(choice == "1")
+        displayMainMenu();
 }
