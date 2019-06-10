4b88480b8412bfaf8ccb0cbe82bca6ca6737115b
Jose Sanchez <jose92ca@gmail.com>
2019-06-10 11:51:27 -0700

    Update 2 (calculator functionality)
    This update adds calculator functionality to the app. It allows the user to perform addition and multiplication with matrices.

--- MatrixAppMain.cpp
+++ MatrixAppMain.cpp
@@ -1,7 +1,6 @@
 /*
  
- This file contains the main program. It is a matrix calculator as well as a matrix operation quiz app. Features and challenges will be added as
- I continue in my studies of linear algebra and the uses of matrices in it.
+ This file contains the main program. It is a matrix calculator as well as a matrix operation quiz app. Features and challenges will be added as I continue in my studies of linear algebra and the uses of matrices in it.
  
  Code Author: Jose Sanchez Rodriguez
  
@@ -14,13 +13,14 @@ void displayMainMenu();
 void displayMatrixOpsMenu();
 void takeAdditionQuiz();
 void takeMultiplicationQuiz();
-
+void calculator();
 
 
 int main()
 {
     cout << "Hello, and welcome to my matrix calculator and quiz app!" << endl;
     displayMainMenu();
+    cout << "\nThank you for using my program! :)" << endl << endl;
     
     return 0;
 }
@@ -33,7 +33,7 @@ void displayMainMenu()
     
     cout << "\nPlease choose from the following options: " << endl << endl;
     cout << "1. Matrix operations quiz" << endl;
-    cout << "2. Matrix calculator (coming soon!)" << endl << endl;
+    cout << "2. Matrix calculator" << endl << endl;
     cout << "Your choice -> ";
     getline(cin, choice);
     
@@ -46,11 +46,11 @@ void displayMainMenu()
         getline(cin, choice);
     }
     
-    if(choice == "2")
-        cout << "\nSorry, that feature has not been fully implemented yet. Check back soon!" << endl;
-    
-    else //must have chose option 1
+    if(choice == "1")
         displayMatrixOpsMenu();
+    
+    else //must have chose option 2
+        calculator();
 }
 
 void displayMatrixOpsMenu()
@@ -248,4 +248,181 @@ void takeMultiplicationQuiz()
     
     if(choice == "1")
         displayMainMenu();
+}
+
+void calculator()
+{
+    string choice;
+    string keepGoing = "y";
+    
+    while(keepGoing == "y")
+    {
+        
+        cout << "\nPlease choose the matrix operation you would like to perform:\n" << endl;
+        cout << "1. Addition" << endl;
+        cout << "2. Multiplication" << endl << endl;
+        cout << "Your choice -> ";
+        getline(cin, choice);
+        
+        while(choice != "1" && choice != "2")
+        {
+            cout << "\n\nInvalid option! Please choose from the options listed:" << endl << endl;
+            cout << "1. Addition" << endl;
+            cout << "2. Multiplication" << endl << endl;
+            cout << "Your choice -> ";
+            getline(cin, choice);
+        }
+        
+        if(choice == "1")
+        {
+            int rows, columns;
+            string input;
+            
+            cout << "\nPlease enter the amount of rows in the matrices you will be adding -> ";
+            getline(cin, input);
+            
+            while(input.find_first_not_of( "0123456789" ) != string::npos || (input.length() >= 2 && input[0] == '0'))
+            {
+                cout << "\nWhoops! Looks like you entered an invalid number." << endl;
+                cout << "Please choose a number greater than or equal to zero." << endl;
+                cout << "Enter the amount of rows in the matrices you will be adding -> ";
+                getline(cin, input);
+            }
+            
+            rows = stoi(input);
+            
+            cout << "\nPlease enter the amount of columns in the matrices you will be adding -> ";
+            getline(cin, input);
+            
+            while(input.find_first_not_of( "0123456789" ) != string::npos || (input.length() >= 2 && input[0] == '0'))
+            {
+                cout << "\nWhoops! Looks like you entered an invalid number." << endl;
+                cout << "Please choose a number greater than or equal to zero." << endl;
+                cout << "Enter the amount of columns in the matrices you will be adding -> ";
+                getline(cin, input);
+            }
+            
+            columns = stoi(input);
+            
+            Matrix a(rows, columns);
+            Matrix b(rows, columns);
+            Matrix sum(rows, columns);
+            
+            cout << "\nFor the first matrix: \n" << endl;
+            a.getUserMatrix();
+            cout << "\n\n";
+            cout << "For the second matrix: \n" << endl;
+            b.getUserMatrix();
+            cout << "\n\n";
+            
+            sum = a + b;
+            
+            a.printMatrix();
+            cout << "\n+\n\n";
+            b.printMatrix();
+            cout << "\n=\n\n";
+            sum.printMatrix();
+            
+        }
+        
+        else
+        {
+            int rowsA, columnsA, columnsB;
+            string input;
+            
+            cout << "\nPlease enter the amount of rows in the first matrix you will be multiplying -> ";
+            getline(cin, input);
+            
+            while(input.find_first_not_of( "0123456789" ) != string::npos || (input.length() >= 2 && input[0] == '0'))
+            {
+                cout << "\nWhoops! Looks like you entered an invalid number." << endl;
+                cout << "Please choose a number greater than or equal to zero." << endl;
+                cout << "Enter the amount of rows in the first matrix you will be adding -> ";
+                getline(cin, input);
+            }
+            
+            rowsA = stoi(input);
+            
+            cout << "\nPlease enter the amount of columns in the first matrix you will be multiplying -> ";
+            getline(cin, input);
+            
+            while(input.find_first_not_of( "0123456789" ) != string::npos || (input.length() >= 2 && input[0] == '0'))
+            {
+                cout << "\nWhoops! Looks like you entered an invalid number." << endl;
+                cout << "Please choose a number greater than or equal to zero." << endl;
+                cout << "Enter the amount of columns in the first you will be multiplying -> ";
+                getline(cin, input);
+            }
+            
+            columnsA = stoi(input);
+            
+            cout << "\nPlease enter the amount of columns in the second matrix you will be multiplying -> ";
+            getline(cin, input);
+            
+            while(input.find_first_not_of( "0123456789" ) != string::npos || (input.length() >= 2 && input[0] == '0'))
+            {
+                cout << "\nWhoops! Looks like you entered an invalid number." << endl;
+                cout << "Please choose a number greater than or equal to zero." << endl;
+                cout << "Enter the amount of columns in the second matrix you will be multiplying -> ";
+                getline(cin, input);
+            }
+            
+            columnsB = stoi(input);
+            
+            Matrix a(rowsA, columnsA);
+            Matrix b(columnsA, columnsB);
+            Matrix product(rowsA, columnsB);
+            
+            cout << "\nFor the first matrix: \n" << endl;
+            a.getUserMatrix();
+            cout << "\n\n";
+            cout << "For the second matrix: \n" << endl;
+            b.getUserMatrix();
+            cout << "\n\n";
+            
+            product = a * b;
+            
+            a.printMatrix();
+            cout << "\n*\n\n";
+            b.printMatrix();
+            cout << "\n=\n\n";
+            product.printMatrix();
+            
+        }
+        
+        cout << "\nWould you like to perform another calculation? ('y' or 'Y' for yes or 'n' or 'N' for no)" << endl;
+        cout << "Your choice -> ";
+        getline(cin, keepGoing);
+        keepGoing[0] = tolower(keepGoing[0]);
+        
+        while(keepGoing != "y" && keepGoing != "n")
+        {
+            cout << "\n\nInvalid option! Please choose from the options listed:" << endl << endl;
+            cout << "Would you like to perform another calculation? ('y' or 'Y' for yes or 'n' or 'N' for no)" << endl;
+            cout << "Your choice -> ";
+            getline(cin, keepGoing);
+            keepGoing[0] = tolower(keepGoing[0]);
+        }
+        
+        if(keepGoing == "n")
+        {
+            cout << "\nWhat would you like to do now?" << endl << endl;
+            cout << "1. Return to main menu" << endl;
+            cout << "2. Quit program" << endl << endl;
+            cout << "Your choice -> ";
+            getline(cin, choice);
+            
+            while(choice != "1" && choice != "2")
+            {
+                cout << "\n\nInvalid option! Please choose from the following options::" << endl << endl;
+                cout << "1. Return to main menu" << endl;
+                cout << "2. Quit program" << endl << endl;
+                cout << "Your choice -> ";
+                getline(cin, choice);
+            }
+            
+            if(choice == "1")
+                displayMainMenu();
+        }
+    }
 }
